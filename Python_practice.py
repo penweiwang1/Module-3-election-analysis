53807c167698882ebee56b73b5e7f59ce574b19d
penweiwang1 <punkwui@PUNKWUIs-MacBook-Air.local>
2022-10-25 22:33:47 -0400

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,11 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+
+voting_data = []
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
